821752a091a5dc708eb9db7c413dc15d63310ab8
Mohammad Moein Hosseini Manesh <moein7tl@gmail.com>
2012-03-06 12:58:37 +0330

    test

---
+++ CaptchaImage.php
@@ -0,0 +1,465 @@
+<?php
+
+/* 
+ * MoeinCaptchaLib
+ */
+
+/**
+ * Description of CaptchaImage
+ * This class is desinged to make image for Captcha code.
+ * This class is reading folder Fonts,you can add any font to this class easily
+ * by copy / past your ttf files into Fonts directory
+ *
+ * @author Seyed Mohammad Moein Hoseini Manesh
+ * @link captcha.printf.ir 
+ * @author mail moein7tl@gmail.com
+ *
+ */
+
+class CaptchaImage {
+    private $fonts = array(); // to keep fonts name
+    private $fonts_dir = 'Fonts';
+    private $width, $height;  // width and height of Image,if you don't use background you can set it directly
+    private $Color_R = null,$Color_G = null,$Color_B = null; // Background RGB
+    private $TC_R = null,$TC_G = null,$TC_B = null;// Text RGB
+    private $HSL_H,$HSL_S,$HSL_L;
+    private $HSL_H_complement;
+    private $Difficultly = 5,$angle = 16;
+    private $BackgroundImage = null;
+    private $ImageSuffix = null;
+    private $font_can_be_use_count;
+    private $font_can_be_user_src = array();
+    protected  $Text;
+    public function  __construct($width = 215,$height = 100) {
+        $this->getFonts();                // get font address in fonts array
+        $this->SetSize($width, $height);  // set size of the Image
+        $this->SetDifficultly(5);         // set difficulty of Captcha/realy its angle
+        $this->setHowManyFontCanBeUse(3); // by default,Captcha code can use 3 fonts
+    }
+
+    public function SetSize($width,$height){
+        /*
+         * default size is 215 * 100,if you dont set it in constructor or by calling this method,it will be 215 * 100
+         * if width is smaller than 50,it will be 215
+         * if height is smaller than 25, it will be 100
+         */
+        $width = round($width);
+        $height = round($height);
+        // check minimum of the size
+        if ($width >= 50)
+            $this->width = $width;
+        else
+            $this->width = 215;
+
+        if ($height >= 25)
+            $this->height = $height;
+        else
+            $this->height = 100;
+    }
+    
+    public function setHowManyFontCanBeUse($font_can_be_use){
+        /*
+         * set how many random fonts can be use in Captcha Image
+         * by default it set 3
+         */
+        if ($font_can_be_use <= count($this->fonts) && $font_can_be_use >= 0)
+            $this->font_can_be_use_count = $font_can_be_use;
+        else
+            $this->font_can_be_use_count = count($this->fonts);
+    }
+
+    public function SetBackGroundImage($address){
+        /*
+         * by calling this method ,you can set Image address
+         * it should be correct image file otherwise it will be set as null
+         */
+        $suffix = substr($address,strrpos($address,"." ,-1) + 1);
+        $suffix = strtolower($suffix);
+        $this->ImageSuffix = $suffix;
+        if (@(is_file($address) && file_exists($address) && ($suffix == "gif" || $suffix == "png" || $suffix == "jpg")))
+            $this->BackgroundImage = $address;
+        else
+            $this->BackgroundImage = null;
+    }
+    
+    public function SetBackGround($red = 255,$green = 255,$blue = 255){
+        /*
+         * set true background
+         * if you don't do it,it will be white as default
+         * you can make random background,by calling this method is uncorrect valua,like 1000,1000,1000
+         */
+        $this->RGBCheker($red, $green, $blue);
+        $this->Color_R = $red;
+        $this->Color_G = $green;
+        $this->Color_B = $blue;
+    }
+    
+    public function SetTextColor($red = 0,$green = 0,$blue = 0){
+        /*
+         * set true text color 
+         * if you don't do it,it will be black as default
+         * you can make random text color,by calling this method is uncorrect valua,like 1000,1000,1000
+         */
+        $this->RGBCheker($red, $green, $blue);
+        $this->TC_R = $red;
+        $this->TC_G = $green;
+        $this->TC_B = $blue;
+    }
+
+    public function SetDifficultly($Difficultly){
+        /*
+         * you can set difficulty from 1 to 10
+         * it will set angle from 0(by 1) to 36(10)
+         * by default,it set to 16(5)
+         */
+        if ($Difficultly >= 1 && $Difficultly <= 10)
+            $this->Difficultly = $Difficultly;
+        else
+            $this->Difficultly = 5;
+        switch ($this->Difficultly){
+            case 1:
+                $this->angle = 0;
+                break;
+            case 2:
+                $this->angle = 4;
+                break;
+            case 3:
+                $this->angle = 8;
+                break;
+            case 4:
+                $this->angle = 12;
+                break;
+            case 5:
+                $this->angle = 16;
+                break;
+            case 6:
+                $this->angle = 20;
+                break;
+            case 7:
+                $this->angle = 24;
+                break;
+            case 8:
+                $this->angle = 28;
+                break;
+            case 9:
+                $this->angle = 32;
+                break;
+            case 10:
+                $this->angle = 36;
+                break;
+            default:
+                $this->angle = 16;
+        }
+        
+    }
+
+    private function getFonts(){
+      // This is method to store fonts name from fonts_dir dircetory(default is Fonts) to fonts array
+      // Return True ,if can find font else return false
+        $count = 0;
+        if ($handle = @opendir($this->fonts_dir)) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file != "." && $file != "..") {
+                    $this->fonts[$count++] = $file;
+                }//end of if , for checking its not . and ..
+            }//end of while
+        }// can open Fonts folder
+        closedir($handle);
+        if ($handle == FALSE)
+            return FALSE;
+        return TRUE;
+    }
+    //+ ------------------------------------------I wanna to call it XPart
+    /*
+     * You can call a color by RGB or HSL
+     * this part is try to find Complementary Color
+     * First,change Background Color to HSL ,then trun Hue value 180 degree
+     * anfter thath,it will change new HSL to RGB for Text Color
+     * you can read more about it in http://serennu.com/colour/rgbtohsl.php
+     */
+    private function RGB2HSL(){
+
+        $red   = $this->Color_R / 255;
+        $green = $this->Color_G /  255;
+        $blue  = $this->Color_B /  255;
+
+        $max = max($red,$green,$blue);
+        $min = min($red,$green,$blue);
+        $difference = $max - $min;
+        $this->HSL_L = ($max + $min) / 2;
+
+        if ($difference == 0){
+            $this->HSL_H = 0;
+            $this->HSL_S = 0;
+        } else {
+            if ($this->HSL_L < 0.5)
+              $this->HSL_S = $difference / ($max + $min);
+            else
+              $this->HSL_S = $difference / (2 - $max - $min);
+
+            $red_temp   = ((($max - $red)   / 6) + ($max / 2)) / $max;
+            $green_temp = ((($max - $green) / 6) + ($max / 2)) / $max;
+            $blue_temp  = ((($max - $blue)  / 6) + ($max / 2)) / $max;
+
+            if ($red == $max)
+                $this->HSL_H = $blue_temp - $green_temp;
+            else if ($green == $max)
+                $this->HSL_H = (1 / 3) + $red_temp - $blue_temp;
+            else if ($blue == $max)
+                $this->HSL_H = (2 / 3) + $green_temp - $red_temp;
+
+            if ($this->HSL_H < 0)
+                    $this->HSL_H += 1;
+            if ($this->HSL_H > 1)
+                    $this->HSL_H -=1;
+
+        }
+        $this->HSL_Complement();
+    }
+    private function HSL_Complement(){
+        $this->HSL_H_complement = $this->HSL_H + 0.5;
+        if ($this->HSL_H_complement > 1)
+                $this->HSL_H_complement--;
+    }
+
+    private function HSL2RGB(){
+        if ($this->HSL_S == 0){
+            $this->TC_R = $this->HSL_L * 255;
+            $this->TC_G = $this->HSL_L * 255;
+            $this->TC_B = $this->HSL_L * 255;
+        } else {
+            if ($this->HSL_L < 0.5)
+                    $var2 = $this->HSL_L * (1 + $this->HSL_S);
+            else
+                    $var2 = ($this->HSL_L + $this->HSL_S) - ($this->HSL_S * $this->HSL_L);
+
+            $var1 = 2 * $this->HSL_L - $var2;
+            $this->TC_R =  255 * $this->HUE2RGB($var1, $var2, $this->HSL_H_complement + (1 / 3));
+            $this->TC_G =  255 * $this->HUE2RGB($var1, $var2, $this->HSL_H_complement);
+            $this->TC_B =  255 * $this->HUE2RGB($var1, $var2, $this->HSL_H_complement - (1 / 3));
+        }
+    }
+    
+    private function HUE2RGB($var1,$var2,$varH){
+        if ($varH < 0)
+            $varH++;
+        if ($varH > 1)
+            $varH--;
+        if ((6 * $varH) < 1)
+            return ($var1 + ($var2 - $var1) * 6 * $varH);
+        if ((2 * $varH) < 1)
+            return ($var2);
+        if ((3 * $varH) < 2)
+            return ($var1 + ($var2 - $var1) * (2 / 3 - $varH) * 6);
+        return ($var1);
+    }
+    //+ --------------------------------------------------- End of the XPart
+    private function getImageMainRGB(){
+        /*
+         * If you set ImageBackground,and dont set background color
+         * it will be call,by these it can set background color
+         * after that find text color to print on this image
+         * these function check 64% of center of image,and get avrage of the RGB color
+         */
+        switch ($this->ImageSuffix){
+            case 'png':
+                $im = @imagecreatefrompng($this->BackgroundImage);
+                break;
+            case 'jpg':
+                $im = @imagecreatefromjpeg($this->BackgroundImage);
+                break;
+            case 'gif':
+                $im = @imagecreatefromgif($this->BackgroundImage);
+                break;
+        }
+        $x = imagesx($im);
+        $y = imagesy($im);
+        $x_start = round(($x / 10));
+        $x_end   = round(9 * ($x / 10));
+        $y_start = round(($y / 10));
+        $y_end   = round(9 * ($y / 10));
+        $sum = array('R' => 0 ,
+                     'G' => 0 ,
+                     'B' => 0);
+        $count = round(($x * $y) / 10); // checking 10% of image (x_end - x_start) * (y_end - y_start) ,center of backgroundImage
+        for ($i = 0; $i < $count; $i++){
+            $rand_x = rand($x_start,$x_end);
+            $rand_y = rand($y_start,$y_end);
+            $rgb = imagecolorat($im, $rand_x, $rand_y);
+            $sum['R'] += ($rgb >> 16) & 0xFF;
+            $sum['G'] += ($rgb >> 8) & 0xFF;
+            $sum['B'] += $rgb & 0xFF;
+        }
+        $this->Color_R = (int)($sum['R'] / $count);
+        $this->Color_G = (int)($sum['G'] / $count);
+        $this->Color_B = (int)($sum['B'] / $count);
+    }
+
+
+    private function RandomRGB(){
+        // make random red,blue or green
+        return (rand(0,255));
+    }
+
+    private function RGBCheker(&$red,&$green,&$blue){
+        //checking RGB color,if it's not true
+        //it will be set by rand
+        if (!($red >= 0 && $red <= 255))
+            $red = $this->RandomRGB();
+        if (!($green >= 0 && $green <= 255))
+            $green = $this->RandomRGB();
+        if (!($blue >= 0 && $green <= 255))
+            $green = $this->RandomRGB();
+    }
+    private function getFontSRC(){
+        //make random font fot imageftbox and imagefttext
+        return $this->fonts_dir."/".$this->font_can_be_user_src[rand(0,$this->font_can_be_use_count - 1)];
+    }
+    private function setFontCanBeUseSRC(){
+        //making random fonts
+        $count = count($this->fonts) - 1;
+        for ($i = 0; $i < $this->font_can_be_use_count ; $i++){
+            $rand = rand(0,$count);
+            if (in_array($this->fonts[$rand], $this->font_can_be_user_src)){
+                //Checking to dont have one font twice or more
+                $i--;
+                continue;;
+            } else {
+                $this->font_can_be_user_src[$i] = $this->fonts[$rand];
+            }
+        }
+    }
+    private function getFontSize_Position($font,$char,$angle,$x){
+        // to set char in correct position with correct font size
+        for ($i = 0;$i < 201;$i++){
+            $pos = imageftbbox($i, $angle, $font, $char);
+            if ($angle >= 0){
+                if ((($pos[1] - $pos[5]) > $this->height || ($pos[2] - $pos[6] > $x)) || $i == 200){
+                    $pos = imageftbbox(--$i, $angle, $font, $char);
+                    $y = ($pos[1] - $pos[5]) + ($this->height - ($pos[1] - $pos[5]))/2 - 5;
+                    return array('size' => $i,
+                                 'YPos' => $y);
+                }
+            } else {
+                if ((($pos[3] - $pos[7]) > $this->height || ($pos[4] - $pos[0] > $x)) || $i == 200){
+                    $pos = imageftbbox(--$i, $angle, $font, $char);
+                    $y = ($pos[3] - $pos[7]) + ($this->height - ($pos[3] - $pos[7]))/2 - ($pos[3] - $pos[1]) - 5;
+                    return array('size' => $i,
+                                 'YPos' => $y);
+                }
+            }
+        }
+    }
+    private function randangle(){
+        //return random angle 
+        return (rand(-1 * $this->angle,$this->angle));
+    }
+    /*
+     * Main of the class
+     */
+    
+    public function draw(){
+        /*
+         * to draw Image of the captcha code
+         */
+        if ($this->BackgroundImage === null){
+            if ($this->Color_B === null || $this->Color_B === null || $this->Color_B === null ){
+                if ($this->TC_B === null || $this->TC_B === null || $this->TC_B === null)
+                        $this->SetTextColor(0, 0, 0);
+                //if obj doen't have background image,background color and text color,it will be set as 0, 0, 0
+            } else {
+                if ($this->TC_B === null || $this->TC_B === null || $this->TC_B === null){
+                        $this->RGB2HSL();
+                        $this->HSL2RGB();
+                        //if obj doesn't have backgorund image,but have background color,and dont have textcolor,it will be set by XPart:D
+                }
+            }
+        } else {
+            if ($this->Color_B === null || $this->Color_B === null || $this->Color_B === null ){
+                $this->getImageMainRGB(); // if obj doesn't background Image,but dont have background color,it will be set by this method
+                if ($this->TC_B === null || $this->TC_B === null || $this->TC_B === null){
+                        $this->RGB2HSL();
+                        $this->HSL2RGB();
+                        //if obj doesn't have TextColor ,it will be set by XPart:D
+                }
+            } else {
+                if ($this->TC_B === null || $this->TC_B === null || $this->TC_B === null){
+                        $this->RGB2HSL();
+                        $this->HSL2RGB();
+                        //XPart again,if TextColor doesnt set,but Background color set
+                }
+            }
+        }
+        // End of Set Background and Text Color
+        $this->setFontCanBeUseSRC();//make random font
+        
+        if ($this->BackgroundImage != null){//have background
+            //create image
+            switch ($this->ImageSuffix){
+                case 'gif':
+                    $im = imagecreatefromgif($this->BackgroundImage);
+                    break;
+                case 'jpg':
+                    $im = imagecreatefromjpeg($this->BackgroundImage);
+                    break;
+                case 'png':
+                default:
+                    $im = imagecreatefrompng($this->BackgroundImage);
+            }
+            $this->width = imagesx($im);
+            $this->height= imagesy($im);
+        } else {    //dont have background
+            $im = imagecreatetruecolor($this->width, $this->height);
+            if (!($this->Color_R === null || $this->Color_G === null || $this->Color_B === null)){
+                $color_Back = imagecolorallocate($im, $this->Color_R , $this->Color_G, $this->Color_B);
+                imagefilledrectangle($im, 0, 0, $this->width, $this->height, $color_Back);
+            } else {
+                //with out background (transparnet image)
+                imagealphablending($im, false);
+                $col=imagecolorallocatealpha($im,255,255,255,127);
+                //Create overlapping 100x50 transparent layer
+                imagefilledrectangle($im,0,0,$this->width, $this->height,$col);
+                //Continue to keep layers transparent
+                imagealphablending($im,true);
+            }
+        }
+        $color_Text = imagecolorallocate($im, $this->TC_R , $this->TC_G, $this->TC_B);//make text color by RGB
+        $count = strlen($this->Text);
+        $XSize =   ($this->width / $count + 5);
+        
+        for ($i = 0; $i < $count; $i++){
+            $char = substr($this->Text,$i,1);
+            $font = $this->getFontSRC();
+            $angle = $this->randangle();
+            $array = $this->getFontSize_Position($font, $char, $angle,  $XSize);
+            imagefttext($im, $array['size'], $angle,  $i * ($XSize - 5) + 5, $array['YPos'], $color_Text,$font, $char);
+            //draw a char into image
+        }
+        
+        switch ($this->ImageSuffix){
+        //send header and draw image
+            case 'jpg':
+                header('Content-type: image/jpeg');
+                imagejpeg($im);
+                break;
+            case 'gif':
+                header('Content-type: image/gif');
+                imagegif($im);
+                break;
+            case 'png':
+                header('Content-type: image/png');
+                imagepng($im);
+                break;
+               default:
+                imagesavealpha($im,true);
+                imagepng($im,"MoeinCaptchaTranspartImage.png",1);
+                header("Content-Type: image/png");
+                readfile("MoeinCaptchaTranspartImage.png");
+                break;
+        }
+        //release a memory
+        imagedestroy($im);
+  }
+    
+}
+?>
---
+++ MoeinCaptcha.php
@@ -0,0 +1,120 @@
+<?php
+
+/*
+ * MoeinCaptchaLib v1.0
+ */
+
+/*
+ * Description of MoeinCaptcha
+ * This class is designed to make random captcha code by your oponion
+ * it extended from CaptchaImage class.
+ * @author Seyed Mohammad Moein Hoseini Manesh
+ * @link captcha.printf.ir 
+ * @author mail moein7tl@gmail.com
+ *  With special thanks to Ali Akbari Boromand and Hossein Bokhamsein
+ */
+
+require_once 'CaptchaImage.php';
+
+class MoeinCaptcha extends CaptchaImage {
+
+    private $probbility_num,$probbility_symbols,$probbility_space;
+    private $len;
+    public function  __construct($width = 215,$height = 100,$len = 6) {
+        parent::__construct($width, $height);
+        $this->setLen($len);           // set length of the captcha code
+        $this->SetProbbility(0, 0, 0); // set probbility of numbers, symboles and space in captcha code
+        $this->newText();              // make random captcha code
+    }
+    
+    private function RandomText($length = 6){
+        /*
+         * This function is returning random captcha code by probbility of numbers,symbols and space
+         */
+        $string = "";
+        $rand = 0;
+        for ($i = 0; $i < $length; $i++){
+            $rand = rand(1, 100);
+            if ($rand >= 1 && $rand <= $this->probbility_num)
+                    $string .= $this->RandomNumber();
+            else if ($rand > $this->probbility_num && $rand <= ($this->probbility_num + $this->probbility_symbols))
+                    $string .= $this->RandomSymbols();
+            else if ($rand > ($this->probbility_num + $this->probbility_symbols) && $rand <= ($this->probbility_num + $this->probbility_symbols + $this->probbility_space))
+                    $string .= " ";
+            else
+                    $string .= $this->RandomAlphaBet();
+        }
+        return $string;
+    }
+    public function SetProbbility($number = 0,$symbol = 0,$space = 0){
+        /*
+         * This method is use to set probbility of numbers ,symbol and space in captcha code
+         * by default,they are 0
+         * if use this ,it will make new captcha code too
+         */
+        settype($number, "int");
+        settype($symbol, "int");
+        settype($space, "int");
+        $number = abs($number);
+        $symbol = abs($symbol);
+        $space = abs($space);
+        if ($number + $symbol + $space <= 100){
+            $this->probbility_num     = $number;
+            $this->probbility_symbols = $symbol;
+            $this->probbility_space = $space;
+        } else {
+            $this->probbility_num     = 0;
+            $this->probbility_symbols = 0;
+            $this->probbility_space   = 0;
+        }
+       $this->newText();
+    }
+
+    private function RandomNumber(){
+        //This method,will return number of char
+        return (chr(rand(48, 57)));
+    }
+    public function newText(){
+        //generate random captcha
+       $this->Text = $this->RandomText($this->len);
+    }
+    public function setLen($len){
+        //set length of captcha code
+        if ($len >= 20 || $len <= 2)
+            $this->len = 6;
+        else
+            $this->len = $len;
+        $this->newText();
+    }
+    private function RandomAlphaBet(){
+        //This method will return a-z /A-Z
+        if (rand(0, 1) == 0) // Captal
+            return (chr(rand(65,90)));
+        else
+            return (chr(rand(97,122)));
+    }
+
+    private function RandomSymbols(){
+        //This method,will return some of symboles
+        $randnum = rand(0,22);
+        if ($randnum <= 5)
+            $randnum += 33;
+        else if ($randnum >= 6 && $randnum <= 9)
+            $randnum += 36;
+        else if ($randnum >= 10 && $randnum <= 16)
+            $randnum += 48;
+        else if ($randnum >= 17 && $randnum <= 19)
+            $randnum += 74;
+        else
+            $randnum += 103;
+        
+        return (chr($randnum));
+    }
+
+    public function getText(){
+        // return captcha code for save in session or insert into database or keep in value
+        // it's better to use it directly befor or after draw method
+        return $this->Text;
+    }
+}
+?>
